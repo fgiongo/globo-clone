5a371014b920fb87cf6d8b681de6c41fcefdb0c6
fgiongo <fernandogiongooliveira@gmail.com>
2021-10-04 19:16:42 -0300

    tentativa de resolver problemas com redimensionamento e cropagem de imagens

--- includes/headline-a-1.php
+++ includes/headline-a-1.php
@@ -1,6 +1,6 @@
 <img class="headline-a-img" src="stock-photos/pexels-aloïs-moubax-2348817.jpg">
 <h3 class="headline-a-eyebrow">Eyebrow key-word(s)</h3>
 <h2 class="headline-a-text">This is a journalism-related headline</h2>
-<ul class="headline-a-ui">
+<ul class="headline-a-ul">
     <li>This is a bullet-point linking to a related story</li>
 </ul>
